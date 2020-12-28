dcc314527023dd46c7eabb714e5bd83ee4df8580
Dani <danibarstad@gmail.com>
2020-12-28 14:25:44 -0600

    trying new js method

--- api/weather.js
+++ api/weather.js
@@ -1,29 +1,38 @@
-var weather;
-var input;
-
-var api = 'api.openweathermap.org/data/2.5/weather?';
-var unit = '&units=imperial'
-var key = '&apiid='
-
-function setup() {
-    var button = select('#submit');
-    button.mousePressed(getWeather);
-
-    c = select('#city');
-    s = select('#state');
+const api = {
+    key: '4c6a7c501ad077318a7c705eea755377',
+    url: 'https://api.openweathermap.org/data/2.5/'
 }
 
-function getWeather() {
-    var request = new XMLHttpRequest();
+const city = document.querySelector('.city');
+// const city = document.getElementById("city");
+city.addEventListener('keypress', setQuery);
+// const state = document.getElementById("state");
 
-    var city = document.getElementById('city').city;
-    var state = document.getElementById('state').state;
 
-    var url = api + city +',' + state + ',US' + unit + key;
-    // var url = api + c.value() +',' + s.value() + ',US' + unit + key;
-    loadJSON(url, gotData);
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(city.value);
+        console.log(city.value);
+    }
 }
 
-function gotData(data) {
-    weather = data;
+function getResults (query) {
+    fetch(`${api.url}weather=?q=${query}&units=imperial&appid=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+    let city = document.querySelector('.location .city');
+    city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>*c</span>`;
+
+    let weather_el = document.querySelector('current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hilow');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}*c / ${Math.round(weather.main.temp_max)}*c`;
 }
