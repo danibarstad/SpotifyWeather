0dded69ac243b6106ff04ed6c8e0ff04125742f4
Dani <danibarstad@gmail.com>
2021-01-03 00:32:21 -0600

    working on API call

--- spotify_weather/spotify_weather_app/api/weather.py
+++ spotify_weather/spotify_weather_app/api/weather.py
@@ -6,17 +6,12 @@ key = os.environ.get('WEATHER_KEY')
 def get_weather(city, state):
     query = {'q': city + ',' + state + ',US', 'units': 'imperial', 'appid': key}
     url = 'https://api.openweathermap.org/data/2.5/weather'
-    try:
-        data = requests.get(url, params=query).json()
-    except KeyError:
-        print(f"KeyError: Please check city name and state code in {query}")
-        return "Error! Please Check city name and state code.", 0, 0, 0
-    weather = data
-    description = weather['description']
-    temperature = weather['main']['temp']
-    cloudy = weather['clouds']['all']
-    humidity = weather['main']['humidity']
-    description = description.capitalize()
+    data = requests.get(url, params=query).json()
+    description = data['description']
+    # temperature = weather['main']['temp']
+    # cloudy = weather['clouds']['all']
+    # humidity = weather['main']['humidity']
+    # description = description.capitalize()
 
     # return description, temperature, cloudy, humidity
     return description
--- spotify_weather/spotify_weather_app/views.py
+++ spotify_weather/spotify_weather_app/views.py
@@ -4,10 +4,10 @@ from .api import weather
 
 # Create your views here.
 def playlist(request):
-    new_weather_form = NewWeatherForm()
-    city = new_weather_form.city
-    state = new_weather_form.state
-    forecast = weather.get_weather(city, state)
+    # new_weather_form = NewWeatherForm()
+    city = request.GET.get('city')
+    state = request.GET.get('state')
+    forecast = weather.get_weather('Austin', 'TX')
 
 
-    return render(request, 'index.html', {'new_weather_form': new_weather_form}) #TODO: change this path
+    return render(request, 'index.html', {'forecast': forecast})
