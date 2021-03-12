6354cc749d10a916965576b9bd37fc1816e4f17f
dani <danibarstad@gmail.com>
2021-03-11 23:14:01 -0600

    successfully calls and displays location weather attributes

--- public/weather.js
+++ public/weather.js
@@ -1,14 +1,36 @@
 let button = document.getElementById('btn');
-let city;
-let state;
+let city = document.getElementById('city').value;
+let state = document.getElementById('state').value;
 const apiKey = '4c6a7c501ad077318a7c705eea755377';  // Regenerate when ready to deploy
 const api_url = 'https://api.openweathermap.org/data/2.5/weather';
 
-button.addEventListener('click', printStuff);
+// button.addEventListener('click', printStuff);
+button.addEventListener('click', getData);
 
 function printStuff() {
-    city = document.getElementById('city').value;
-    state = document.getElementById('state').value;
     console.log(city);
     console.log(state);
+}
+
+function getData() {
+    fetch(`${api_url}?q=${city},${state},US&units=imperial&appid=${apiKey}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            printData(data);
+        });
+}
+
+function printData(data) {
+    // console.log(data);
+    let description = data['weather'][0]['description'];
+    let temperature = data['main']['temp'];
+    let humidity = data['main']['humidity'];
+    let windSpeed = data['wind']['speed'];
+    console.log(`Description: ${description}`);
+    console.log(`Temperature: ${temperature}`);
+    console.log(`Humidity: ${humidity}`);
+    console.log(`Wind Speed: ${windSpeed}`);
+
 }
