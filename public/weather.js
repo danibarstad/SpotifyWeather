ae3c9ea0ff85594cabc7d65595e734bfa1bea626
dani <danibarstad@gmail.com>
2021-03-11 23:31:33 -0600

    added name and attribute descriptions

--- public/weather.js
+++ public/weather.js
@@ -1,6 +1,12 @@
 let button = document.getElementById('btn');
 let city = document.getElementById('city').value;
 let state = document.getElementById('state').value;
+
+let desc = document.getElementById('desc');
+let temp = document.getElementById('temp');
+let humid = document.getElementById('humid');
+let wind = document.getElementById('wind');
+
 const apiKey = '4c6a7c501ad077318a7c705eea755377';  // Regenerate when ready to deploy
 const api_url = 'https://api.openweathermap.org/data/2.5/weather';
 
@@ -24,13 +30,19 @@ function getData() {
 
 function printData(data) {
     // console.log(data);
+    let name = data['name'];
     let description = data['weather'][0]['description'];
     let temperature = data['main']['temp'];
     let humidity = data['main']['humidity'];
     let windSpeed = data['wind']['speed'];
+    console.log(name);
     console.log(`Description: ${description}`);
     console.log(`Temperature: ${temperature}`);
     console.log(`Humidity: ${humidity}`);
     console.log(`Wind Speed: ${windSpeed}`);
 
+    desc.innerHTML = `Description: ${description}`;
+    temp.innerHTML = `Temperature: ${temperature}`;
+    humid.innerHTML = `Humidity: ${humidity}`;
+    wind.innerHTML = `Wind Speed: ${windSpeed}`;
 }
