76ac1227e202615a49c648c18356b2fd5b06eb86
dani <danibarstad@gmail.com>
2021-04-09 15:26:08 -0500

    code cleanup

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -2,26 +2,12 @@ let login_btn = document.getElementById('login');
 let redirectUri = 'http://localhost:5501/public/loggedin.html';
 let scopes = 'user-read-private';
 
-let username = document.getElementById('username');
-let followers = document.getElementById('followers');
-let email = document.getElementById('email');
-let country = document.getElementById('country');
-let product = document.getElementById('product');
-
-// button.addEventListener('click', printInput);
-// button.addEventListener('click', displayInput);
-login_btn.addEventListener('click', () => {
-    access_token = getAccess();
-    getData(access_token);
-});
-login_btn.addEventListener('click', getData);
-
-function printInput() {
-
-}
-
-function displayInput() {
-
+if (login_btn) {
+    login_btn.addEventListener('click', () => {
+        access_token = getAccess();
+        getData(access_token);
+    });
+    login_btn.addEventListener('click', getData);
 }
 
 function getData(access_token) {
@@ -36,18 +22,13 @@ function getData(access_token) {
         })
         .then(function(data) {
             console.log(data);
-            // displayData(data);
         });
-}
-
-function displayData(data) {
-
 }
 
 function getAccess() {
     const responseType = 'token';
-    const CLIENT_ID_TEMP /*= {ADD CLIENT ID HERE} */;
-    let url = `https://accounts.spotify.com/authorize?response_type=${responseType}&client_id=${CLIENT_ID_TEMP}&scope=${scopes}&redirect_uri=${redirectUri}`;
+    const CLIENT_ID = '16cf13c37e984102ac0cd4c7a14e095c';
+    let url = `https://accounts.spotify.com/authorize?response_type=${responseType}&client_id=${CLIENT_ID}&scope=${scopes}&redirect_uri=${redirectUri}`;
     window.location = url;
     return getHashParams();
 }
--- scripts/weather.js
+++ scripts/weather.js
@@ -10,13 +10,7 @@ let wind = document.getElementById('wind');
 const apiKey = '4c6a7c501ad077318a7c705eea755377';  // Regenerate when ready to deploy
 const api_url = 'https://api.openweathermap.org/data/2.5/weather';
 
-// button.addEventListener('click', printStuff);
 button.addEventListener('click', getData);
-
-function printStuff() {
-    console.log(city);
-    console.log(state);
-}
 
 function getData() {
     fetch(`${api_url}?q=${city},${state},US&units=imperial&appid=${apiKey}`)
@@ -29,17 +23,10 @@ function getData() {
 }
 
 function printData(data) {
-    // console.log(data);
-    let name = data['name'];
     let description = data['weather'][0]['description'];
     let temperature = data['main']['temp'];
     let humidity = data['main']['humidity'];
     let windSpeed = data['wind']['speed'];
-    console.log(name);
-    console.log(`Description: ${description}`);
-    console.log(`Temperature: ${temperature}`);
-    console.log(`Humidity: ${humidity}`);
-    console.log(`Wind Speed: ${windSpeed}`);
 
     desc.innerHTML = `Description: ${description}`;
     temp.innerHTML = `Temperature: ${temperature}`;
